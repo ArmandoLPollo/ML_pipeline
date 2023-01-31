c138043338f26441cb1388fe22acf533aea18949
Anthony REINO JOAQUIM <anthonyjoaquim40@gmail.com>
2023-01-31 16:06:16 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+
+# Load the trained model
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+@app.route("/classify", methods=["POST"])
+def classify():
+    # Read the data from the request
+    data = request.get_json()
+    pixels = np.array(data["pixels"]).reshape(1, -1)
+    
+    # Use the model to make a prediction
+    pred = model.predict(pixels)[0]
+    
+    # Return the result as a JSON response
+    return {"class": int(pred)}
+
+if __name__ == "__main__":
+    app.run()
---
+++ test_main.py
@@ -0,0 +1,22 @@
+import unittest
+import requests
+from app import app
+
+class FlaskTestCase(unittest.TestCase):
+
+    def create_app(self):
+        return app
+
+    def test_classify(self):
+        input_data = {"pixels": [0 for i in range(784)]}
+
+        response = requests.post("http://localhost:5000/classify", json=input_data)
+
+        result = response.json()
+        expected_result = {"class": 0}
+
+        assert result == expected_result, f"Expected {expected_result} but got {result}"
+        print("Test passed!")
+    
+if __name__ == '__main__':
+    unittest.main()
